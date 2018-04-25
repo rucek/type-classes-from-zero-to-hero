1910f41dde802526c4fca4eb546e3f6329512e96
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2018-04-25 22:58:02 +0200

    Adds Show instances for Int and String

--- src/main/scala/org/kunicki/typeclasses/Show.scala
+++ src/main/scala/org/kunicki/typeclasses/Show.scala
@@ -4,3 +4,18 @@ trait Show[T] {
 
   def show(t: T): String
 }
+
+object Show {
+
+  implicit val showInt: Show[Int] = (i: Int) => s"int: $i"
+
+  implicit val showString: Show[String] = (t: String) => s"""string: "$t""""
+}
+
+object ShowTest extends App {
+
+  import Show._
+
+  println(showInt.show(42))
+  println(showString.show("foo"))
+}
